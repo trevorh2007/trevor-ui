a17c5193942957504592b974a6346236e3397fb8
Trevor Howard <trevorhoward2@gmail.com>
2025-10-24 03:20:02 -0600

    Remove building library before setting up pages, this was overwriting the demo app build in the dist folder

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {
+      config: './config/tailwind.config.js',
+    },
     autoprefixer: {},
   },
 };
--- tailwind.config.js
+++
@@ -1,3 +0,0 @@
-// Forward to config directory
-import config from './config/tailwind.config.js';
-export default config;
